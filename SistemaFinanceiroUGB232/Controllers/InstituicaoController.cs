106388e24a535ad63dea08ff3403a9136dfe2818
Gabriel Miranda <gbmonovaes@gmail.com>
2023-10-24 20:46:56 -0300

    24/10/2023

---
+++ SistemaFinanceiroUGB232/Controllers/InstituicaoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaFinanceiroUGB232.Models;
+
+namespace SistemaFinanceiroUGB232.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "Hogwarts",
+                Endereco = "Escocia",
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "Mansão X",
+                Endereco = "Nova York",
+            }
+        };
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Instituicao instituicao)
+        {
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+    }
+}
---
+++ SistemaFinanceiroUGB232/Models/Instituicao.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SistemaFinanceiroUGB232.Models
+{
+    public class Instituicao
+    {
+        [DisplayName("Código")]
+        public long? InstituicaoID { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }    
+    }
+}
